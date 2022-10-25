888ee07c22c51a0020a335267e2019678f2d6fb5
SowmyaGorripati <116424393+SowmyaGorripati@users.noreply.github.com>
2022-10-25 23:13:32 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,56 @@
+let resume={
+    "basics": {
+      "name": "Sowmya Gorripati",
+      "email": "sowmygorripati@gamil.com",
+      "phone": 9515968152,
+      "degree": "BTech",
+      "location": {
+        "address": "Flat.no:102, Anala residency, Santosh Nagar",
+        "postalCode": 501332,
+        "city": "Rajahmundry",
+        "state": "Andhra Pradesh",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "LinkedIN": "https://www.linkedin.com/in/sowmyagorripati/",
+          "github":"https://github.com/SowmyaGorripati"
+        }
+      ]
+    },
+    "Work": [
+      {
+        "Company": "Accenture",
+        "Position": "Application Development associate",
+        "StartDate": "3-Sep 2021",
+        "Summary": "I m working under Capital Markets",
+      },
+    ],
+    "Education": [
+      {
+        "Institution": "Sir Cr Reddy college of Engineering",
+        "Department": "Information Technology",
+        "Start year": 2017,
+        "End year": 2021,
+        "GPA": 7.4,
+      }
+    ],
+    "Skills": [
+      {
+        "Programming Languages": "python,javascript",
+        "level": "beginer",
+        
+      }
+    ],
+    "Languages": [
+      {
+        "Language": "Telugu,Hindi, English",
+      }
+    ],
+    "Interests": [
+      {
+        "name": "Pencil sketching",
+      }
+    ]
+  }
+  console.log(resume);
